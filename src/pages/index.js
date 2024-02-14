997028b0029c6675e2f956366afc9cff09f39611
Or-10 <omraj132002@gmail.com>
2024-02-14 22:53:56 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import Parallax from '@/components/Parallax'
+
+function Home() {
+  return (
+    <>
+      <Parallax />
+      <div className='about'>
+        <h2>
+          Mental Peace
+        </h2>
+        <p>Inner peace, or mental peace, is a state of serenity and calmness within oneself, even amidst the chaos and stress of everyday life. It is possible to cultivate inner peace through various practices, and you don't necessarily need to meditate on a mountaintop or spend a fortune on a wellness retreat to find it. Here are some micro-practices that can help you tap into your inner calm:</p>
+        <ul>
+          <li>
+            Practice the 4-7-8 breath: This yoga-based breathing technique involves inhaling for a count of four, holding the breath for a count of seven, and exhaling for a count of eight. The long exhale helps stimulate the parasympathetic nervous system, which initiates a relaxation response in your body.</li>
+          <li>
+          Focus on your breath: Simply paying attention to your breath and reminding yourself that you're breathing can help calm your mind and bring you back to the present moment.
+          </li>
+          <li>
+          Visualize a happy place: Close your eyes and imagine yourself in a peaceful, calming environment. Use all your senses to make the experience as vivid as possible.
+          </li>
+          <li>
+          Practice self-compassion: Recognize and acknowledge your suffering, connect with all of humanity, and talk kindly to yourself. Treat yourself as you would a dear friend.
+          </li>
+          <li>
+          Engage in activities that bring you joy: Take a few moments to savor the things that bring you happiness and rewire your brain for positivity.
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Home
